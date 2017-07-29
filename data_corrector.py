e90f3b29e4a9e4740771f63f2e9987431a8d25c8
Pinaki Nath Chowdhury <pinakinathc@gmail.com>
2017-07-29 23:04:55 +0530

    corrected some errors

--- data_corrector.py
+++ data_corrector.py
@@ -5,13 +5,19 @@ with open('data.pickle', 'rb') as f:
 	temp = pickle.load(f)
 	f.closed
 
-for i, var in enumerate(temp['article']):
+counter = 0
+
+for i, var in enumerate(temp['target']):
 	if len(var)!=7:
+		counter += 1
+		print "index and target: ",i, var
 		del temp['article'][i]
 		del temp['article_publisher'][i]
 		del temp['article_year'][i]
 		del temp['article_state'][i]
 		del temp['target'][i]
+
+print "No of entries deleted due to wrong target values: ",counter
 
 with open('data.pickle', 'wb') as f:
 	pickle.dump(temp, f, pickle.HIGHEST_PROTOCOL)
